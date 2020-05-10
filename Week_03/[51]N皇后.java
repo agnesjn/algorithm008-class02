ca9b46c7154e4927006f09e76d4ab7494d28f2a4
agnesjn <45088442+agnesjn@users.noreply.github.com>
2020-05-10 22:27:18 +0800

    Week 03

---
+++ Week_03/[51]N皇后.java
@@ -0,0 +1,102 @@
+//n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。 
+//
+// 
+//
+// 上图为 8 皇后问题的一种解法。 
+//
+// 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。 
+//
+// 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。 
+//
+// 示例: 
+//
+// 输入: 4
+//输出: [
+// [".Q..",  // 解法 1
+//  "...Q",
+//  "Q...",
+//  "..Q."],
+//
+// ["..Q.",  // 解法 2
+//  "Q...",
+//  "...Q",
+//  ".Q.."]
+//]
+//解释: 4 皇后问题存在两个不同的解法。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 皇后，是国际象棋中的棋子，意味着国王的妻子。皇后只做一件事，那就是“吃子”。当她遇见可以吃的棋子时，就迅速冲上去吃掉棋子。当然，她横、竖、斜都可走一或七步
+//，可进可退。（引用自 百度百科 - 皇后 ） 
+// 
+// Related Topics 回溯算法
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    int rows[];
+    int hills[];
+    int dales[];
+    int n;
+    List<List<String>> output = new ArrayList();
+    int queens[];
+
+    public boolean isNotUnderAttack(int row, int col) {
+        int res = rows[col] + hills[row - col + 2 * n] + dales[row + col];
+        return (res == 0) ? true : false;
+    }
+
+    public void placeQueen(int row, int col) {
+        queens[row] = col;
+        rows[col] = 1;
+        hills[row - col + 2 * n] = 1;
+        dales[row + col] = 1;
+    }
+
+    public void removeQueen(int row, int col) {
+        queens[row] = 0;
+        rows[col] = 0;
+        hills[row - col + 2 * n] = 0;
+        dales[row + col] = 0;
+    }
+
+    public void addSolution() {
+        List<String> solution = new ArrayList<String>();
+        for (int i = 0; i < n; ++i) {
+            int col = queens[i];
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < col; ++j) sb.append(".");
+            sb.append("Q");
+            for(int j = 0; j < n - col - 1; ++j) sb.append(".");
+            solution.add(sb.toString());
+        }
+        output.add(solution);
+    }
+
+    public void backtrack(int row) {
+        for (int col = 0; col < n; col++) {
+            if (isNotUnderAttack(row, col)) {
+                placeQueen(row, col);
+                if (row + 1 == n) addSolution();
+                else backtrack(row + 1);
+                removeQueen(row, col);
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        this.n = n;
+        rows = new int[n];
+        hills = new int[4 * n - 1];
+        dales = new int[2 * n - 1];
+        queens = new int[n];
+
+        backtrack(0);
+        return output;
+    }
+}
+
